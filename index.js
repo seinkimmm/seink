1c58a37db694c633c79608711a80643aba4fd81f
seinkim <seinkim@umich.edu>
2020-05-23 19:23:38 -0400

    new

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-object.addEventListener("scroll", myScript);
+//
+// var div = $("#aboutMe");
+// var home = $("#home");
+//
+//   var pos = div.position();
+//   var homePos = home.position();
+//
+//   $(window).scroll(function() {
+//     var windowpos = $(window).scrollTop();
+//     if (windowpos > pos.top-300) {
+//       div.show();
+//
+//     } else {
+//        div.addClass("BeforeScroll");
+//      }
+//     //   // div.removeClass("AfterScroll");
+//     // }
+//   });
+
+setTimeout( function(){$('.first').show();} , 3800);
+setTimeout( function(){$('.homeImg').hide();} , 3500);
