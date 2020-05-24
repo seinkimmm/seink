e605ef1109cb24356cf32ca45821450ab437f4e4
seinkim <seinkim@umich.edu>
2020-05-24 19:47:31 -0400

    day 1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//
+
 // var div = $("#aboutMe");
 // var home = $("#home");
 //
@@ -14,8 +14,13 @@
 //        div.addClass("BeforeScroll");
 //      }
 //     //   // div.removeClass("AfterScroll");
-//     // }
+//     // }<i class="fas fa-folder-open"></i>
 //   });
+$('button').click(function(){
+  $(this).find("i").toggleClass("fa-folder-open");
+});
 
-setTimeout( function(){$('.first').show();} , 3800);
+setTimeout( function(){$('.first').show();} , 3500);
+setTimeout( function(){$('#aboutMe').show();} , 3800);
+setTimeout( function(){$('#skills').show();} , 3800);
 setTimeout( function(){$('.homeImg').hide();} , 3500);
