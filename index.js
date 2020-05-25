5a61ac8ac6a6d64d3da153292d0920449f74cc0e
seinkim <seinkim@umich.edu>
2020-05-25 15:24:57 -0400

    day 2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $('button').click(function(){
   $(this).find("i").toggleClass("fa-folder-open");
 });
 
-setTimeout( function(){$('.first').show();} , 3500);
+setTimeout( function(){$('.first').show();} , 500);
 setTimeout( function(){$('#aboutMe').show();} , 3800);
 setTimeout( function(){$('#skills').show();} , 3800);
 setTimeout( function(){$('.homeImg').hide();} , 3500);
